29955e9d2a6f540eabc0fabaa712d20a2f286f03
Xiaoru Li <thexrcs@gmail.com>
2017-11-13 18:17:57 +0100

    Manual setup async/await support backup

--- js/App.js
+++ js/App.js
@@ -1,1 +1,13 @@
-console.log("Cool");
+async function fetchContent() {
+  const url =
+    "https://en.wikipedia.org/w/api.php?action=query&titles=List_of_computer_technology_code_names&prop=revisions&rvprop=content&format=json&origin=*";
+  const content = await fetch(url)
+    .then(resp => resp.json())
+    .catch(error => {
+      console.log(JSON.stringify(error));
+    });
+  console.log(content);
+  return content;
+}
+
+console.log(fetchContent());
